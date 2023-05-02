dc283620a44712edf37f9d32d5fdf2bf86a88871
Salman-165324 <salmansadi165324@gmail.com>
2023-05-03 04:34:53 +0600

    created allChefsroute and showed data in it

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require('express');
 const app = express() 
 const port = 5000; 
 
+const allChefs = require('./data/chefs.json')
+
 app.get('/', (req, res) => {
 
     res.send("Welcome to WeChef"); 
 })
+
+app.get('/allChefs', (req, res) => {
+
+    res.send(allChefs); 
+})
+
 
 
 app.listen(port, () => {
